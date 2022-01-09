d41bbdadb4f7074f54991f011c1ec9703cf880c2
Emmy <60683173+emmy-html@users.noreply.github.com>
2022-01-09 02:45:32 -0600

    Added basic javascript functiionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+import data from './data.json';
+// mobile menu
 const menuButton = document.getElementById("menu-toggle-button"); // use the DOM to access the hamburger button
 function hamburger() {
     // declare all variables
@@ -12,7 +14,34 @@ function hamburger() {
     }
 }
 menuButton.addEventListener("click", hamburger);
+// mobile planet structure navigation
+let planetOverviewBtn, planetStructureBtn, planetSurfaceBtn, planetContentContainer, planetSrc; // declare all buttons, containers, and links
+planetOverviewBtn = document.getElementById("planet-overview-button");
+planetStructureBtn = document.getElementById("planet-internal-structure-button");
+planetSurfaceBtn = document.getElementById("planet-surface-geology-button");
+planetContentContainer = document.getElementById("planet-content-container");
+planetSrc = document.getElementById("planet-source");
 
-document.addEventListener('DOMContentLoaded', function() {
-    // your code here
- }, false);
+const body = document.body;
+
+function getPlanetDataByType(dataType) {
+    let currentPlanet = body.dataset.planet;  // check body to see what data is being displayed
+    let match = data.find(planet => planet.name === currentPlanet);
+    if (dataType === "overview") {
+        planetContentContainer.innerHTML = match.overview.content;
+        planetSrc.href = match.overview.source;
+        toggleActive(planetOverviewBtn);
+    }
+    else if (dataType === "structure") {
+        planetContentContainer.innerHTML = match.structure.content;
+        planetSrc.href = match.structure.source;
+    }
+    else if (dataType === "geology") {
+        planetContentContainer.innerHTML = match.geology.content;
+        planetSrc.href = match.geology.source;
+    }
+}
+
+planetOverviewBtn.addEventListener("click", getPlanetDataByType.bind(this, 'overview'));
+planetStructureBtn.addEventListener("click", getPlanetDataByType.bind(this, 'structure'));
+planetSurfaceBtn.addEventListener("click", getPlanetDataByType.bind(this, 'geology'));
